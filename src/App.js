cfed0bde0c9afee46fa5f39372fcb5fc2d75ab90
Délanno Kotcho <delanktc24@gmail.com>
2024-12-23 18:50:00 +0100

    initialisation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-red-300">salut mon blog</p>
+    </div>
+  );
+}
+
+export default App;
